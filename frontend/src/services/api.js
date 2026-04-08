2e1e0971dfdb7ce87ed7877b12d9d7efaa7985d7
Shreyapatil2142 <shreyapatil2142@gmail.com>
2026-04-09 02:23:20 +0530

    Error solved

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with your production URL later
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 // Products
